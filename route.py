c7731bf7c884891884482a32db1d87b7a38b2089
Daniel F. Moisset <dmoisset@machinalis.com>
2010-09-09 10:19:06 -0700

    Get object or 404

--- route.py
+++ route.py
@@ -1,6 +1,7 @@
 from django.core import urlresolvers
 from django.conf.urls.defaults import url
 from django.db.models.loading import get_model
+from django.shortcuts import get_object_or_404
 
 import functools, re
 
@@ -17,7 +18,7 @@ def object_mapper(app, model, query):
     # find mapping for query
     mapping = mapping_str # FIXME: find right mapping from query
     # FIXME: probably we want get_object_or_404 here
-    return lambda value: model_class._default_manager.get(**{query: mapping(value)})
+    return lambda value: get_object_or_404(model_class, **{query: mapping(value)})
 
 def queryset_mapper(app, model, query):
     # get model by name
